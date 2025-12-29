1bf84db74aaaa5dc5d358f73a8c1e873357d2ed7
Justin Kamper <jkamper@fulcrumgt.com>
2025-12-29 23:22:44 +0000

    example usage for testing

---
+++ example_usage.py
@@ -0,0 +1,12 @@
+from PromptLog import *
+
+
+@promptlog(context_window=5, mask_secrets=True)
+def useless_func(var1, var2):
+    print(var1, var2)
+    var1 = int(var1)  # trigger some error
+    var2 = var1
+    return var1, var2
+
+
+useless_func("frfr", "ong")
